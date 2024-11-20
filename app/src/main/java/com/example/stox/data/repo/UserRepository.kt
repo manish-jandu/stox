24673317fa7d2ae0a40574fd17e597a65138e761
manish-jandu <manishjandu1@gmail.com>
2024-11-20 20:01:03 +0530

    user repo setup

---
+++ app/src/main/java/com/example/stox/data/repo/UserRepository.kt
@@ -0,0 +1,37 @@
+package com.example.stox.data.repo
+
+import com.example.stox.data.NetworkUtils.Resource
+import com.example.stox.data.api.ApiService
+import com.example.stox.data.model.Holding
+import com.example.stox.data.model.HoldingData
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class UserRepository @Inject constructor(
+    private val apiService: ApiService
+) {
+
+    suspend fun getUserHoldings(): Resource<List<Holding>> {
+        return try {
+            val response = apiService.getUserHoldings()
+            Resource.Success(response.data.userHolding)
+        } catch (e: Exception) {
+            when (e) {
+                is HttpException -> {
+                    Resource.Error("Server error: ${e.message()}")
+                }
+                is IOException -> {
+                    Resource.Error("Network error: ${e.message}")
+                }
+                else -> {
+                    Resource.Error("Unexpected error: ${e.localizedMessage}")
+                }
+            }
+        }
+    }
+
+
+}
